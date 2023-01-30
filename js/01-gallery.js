13dad110b220654d179b4b0907a53d644148eb4b
Stanislav <stanislav.nechaev@gmail.com>
2023-01-30 22:08:19 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,53 +2,32 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const gallery = document.querySelector('.gallery')
-const items = []
-
-galleryItems.forEach(element => {
-  const galleryItem = document.createElement('div')
-  galleryItem.className = 'gallery__item'
-
-  const galleryLink = document.createElement('a')
-  galleryLink.className = 'gallery__link'
-  galleryLink.href = element.original
-
-  const galleryImage = document.createElement('img')
-  galleryImage.className = 'gallery__image'
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute('data-source', element.original)
-  galleryImage.alt = element.description;
-
-  galleryItem.append(galleryLink)
-  galleryLink.append(galleryImage)
-  items.push(galleryItem)
-})
-
-gallery.append(...items)
-
-gallery.addEventListener('click', e => {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return
+const makeEl = (classes = "", child = null, tag = "div") => {
+  const nodeEl = document.createElement(tag);
+  nodeEl.className = classes;
+  if (child) {
+    nodeEl.append(child);
   }
-  const selectedImage = e.target.getAttribute('data-source')
-  const instance = basicLightbox.create(`
-    <img src="${selectedImage}" width="800" height="600">
-`, {
-    onClose: () => { 
-    console.log("imgClouse");
-    gallery.removeEventListener('keydown', keydownHandler )
-    },
-    onShow: () => {
-      console.log("imgOpened");
-    }
-  })
-
-  instance.show()
-  
+  return nodeEl;
+}
+const openImg = (e, src) => {
+  e.preventDefault();
   const keydownHandler = e => {
     if (e.key === 'Escape') {
       instance.close()
+      gallery.removeEventListener('keydown', keydownHandler)
     }
   }
+  const instance = basicLightbox.create(`<img src="${src}" width="800" height="600">`);
   gallery.addEventListener('keydown', keydownHandler)
-})
+  instance.show()
+}
+galleryItems.forEach(({ preview, original, description }) => {
+  const galleryImage = makeEl("gallery__image", null, "img");
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+  const galleryLink = makeEl('gallery__link', galleryImage, "a");
+  galleryLink.href = original;
+  galleryImage.addEventListener("click",(e) => openImg(e, original))
+  gallery.append(makeEl('gallery__item', galleryLink))
+}) 
